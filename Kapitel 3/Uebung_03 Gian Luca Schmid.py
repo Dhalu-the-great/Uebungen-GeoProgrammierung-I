e87809b15d1deb9e5e9b8c4566ccb3cb5f5cdc48
Dhalu-the-Great <doesel1000@gmx.ch>
2023-03-30 11:41:29 +0200

    Update Uebung_03 Gian Luca Schmid.py

--- Kapitel 3/Uebung_03 Gian Luca Schmid.py
+++ Kapitel 3/Uebung_03 Gian Luca Schmid.py
@@ -1,3 +1,4 @@
+from math import *
 class Figur:
     def __init__(self, name):
         self.name = name
@@ -56,9 +57,45 @@ class Dreieck(Figur):
     def umfang(self):
         return self.A.dist(self.B) + self.B.dist(self.C) + self.C.dist(self.A)
 
+class Rechteck(Figur):
+    def __init__(self,ax,ay,bx,by,cx,cy,dx,dy):
+        super().__init__("Rechteck")
+        self.A = Punkt(ax,ay)
+        self.B = Punkt(bx,by)
+        self.C = Punkt(cx,cy)
+        self.D = Punkt(dx,dy)
+
+
+
+    def umfang(self):
+        return self.A.dist(self.B)+self.B.dist(self.C)+self.C.dist(self.D)+self.D.dist(self.A)
+
+    def flaeche(self):
+        v1 = (self.B - self.A)
+        v2 = (self.D - self.A)
+        v3 = (self.B - self.C)
+        v4 = (self.D - self.C)
+        return v1.cross(v2) / 2 + v3.cross(v4)/2
+
+class Kreis (Figur):
+    def __init__ (self,mx,my,r):
+        super().__init__("Kreis")
+        self.m=Punkt(mx,my)
+        self.r=r
+        
+    def flaeche(self):
+        return self.r**2*pi
+    
+    def umfang(self):
+        return self.r*2*pi
 
 p = Punkt(1, 2)
 d = Dreieck(0, 0, 0, 2, 2, 2)
-
+r=Rechteck(0,0,2,0,2,2,0,2)
+k=Kreis(0,0,10)
 print(d.umfang())
 print(d.flaeche())
+print(r.umfang())
+print(r.flaeche())
+print(k.flaeche())
+print(k.umfang())
