19dafbe60f296e5c08e1270a5fccf51da3ff7885
Dhalu-the-Great <doesel1000@gmx.ch>
2023-04-25 11:39:09 +0200

    verschiedenes

--- Kapitel 5/Uebung 5.py
+++ Kapitel 5/Uebung 5.py
@@ -1,8 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-import sys
-import os
+
 
 class fenster(QMainWindow):
     def __init__(self):
--- Kapitel 7/Vorlesung.py
+++ Kapitel 7/Vorlesung.py
@@ -1,2 +1,10 @@
 import matplotlib.pyplot as plt
 from math import *
+
+
+x=[]
+y=[]
+for i in range(100000):
+    value=i/100000
+    x.append(value)
+    y.append(value*/)
