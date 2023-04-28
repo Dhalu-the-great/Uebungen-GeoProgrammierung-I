6d742d457126af8db60fb1ce9686b745f1d67ff7
Dhalu-the-Great <doesel1000@gmx.ch>
2023-04-28 17:41:03 +0200

    uebung 7 teil 2

--- Kapitel 7/Uebung 7 Teil 2.py
+++ Kapitel 7/Uebung 7 Teil 2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 def f(x, y):
     return np.exp(-x**2) * np.sin(y)
 
@@ -12,5 +13,13 @@ y=np.linspace(-10,10,1000)
 X, Y = np.meshgrid(x, y)
 
 Z=f(X,Y)
-plt.figure(X,Y,Z)
+
+plt.pcolormesh(X,Y,Z )
+plt.colorbar()
+plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(X, Y, Z)
+
 plt.show()
