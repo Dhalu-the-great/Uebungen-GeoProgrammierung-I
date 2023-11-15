b91b44576eb45d4e8c5728c864372f49622da4ef
Dhalu-the-Great <doesel1000@gmx.ch>
2023-11-15 13:45:24 +0100

    Update kapitel10.py

--- kapitel 10/kapitel10.py
+++ kapitel 10/kapitel10.py
@@ -1,17 +1,5 @@
-from qgis.core import *
-import qgis.utils
-
-canvas = iface.mapCanvas()
-layers = canvas.layers()
-
-features = airports.getFeatures()
-alles=list(features)
-file=open("C:/Users/doese/Documents/GitHub/Uebungen-GeoProgrammierung-I/kapitel 10/airports.csv","w",encoding="utf-8")
-for data in alles:
-    name = data.attributes()[4]
-    wikipedia = data.attributes()[9]
-    geometrie = data.geometry().asPoint()
-    file.write(f"{name},{wikipedia},{geometrie.x()},{geometrie.y()}\n")
+from qgis import processing
+for alg in QgsApplication.processingRegistry().algorithms():
+    print(alg.id(), "->", alg.displayName())
+    processing.algorithmHelp("Model:Import and Filter csv")
     
-file.close()
-print("done")
